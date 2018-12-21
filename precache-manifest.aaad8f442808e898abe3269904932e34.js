b96f7043ed7493f534d9936fd62c8f27a863b846
Citlalli DMG <citlallidmg@gmail.com>
2018-12-20 23:18:18 -0600

    Updates

---
+++ precache-manifest.aaad8f442808e898abe3269904932e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b119c431745c1f7815c4",
+    "url": "/redux-example/static/js/runtime~main.b119c431.js"
+  },
+  {
+    "revision": "2efd3e8075fcda88460b",
+    "url": "/redux-example/static/js/main.2efd3e80.chunk.js"
+  },
+  {
+    "revision": "f0a4c6aaeded379de790",
+    "url": "/redux-example/static/js/1.f0a4c6aa.chunk.js"
+  },
+  {
+    "revision": "2efd3e8075fcda88460b",
+    "url": "/redux-example/static/css/main.2a9508a0.chunk.css"
+  },
+  {
+    "revision": "8b93c8c7a522beac1bde6eab3156bab0",
+    "url": "/redux-example/index.html"
+  }
+];
